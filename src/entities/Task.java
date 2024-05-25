bd1632504c1d66cb64800f78589721513689976f
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-05-25 17:35:11 -0300

    Add Task, Priority and Status

--- src/entities/Task.java
+++ src/entities/Task.java
@@ -1,4 +1,65 @@
 package entities;
 
+import entities.enums.Priority;
+import entities.enums.Status;
+
+import java.time.LocalDate;
+
 public class Task {
+  private String title;
+  private String description;
+  private LocalDate dueDate;
+  private Priority priority;
+  private Status status;
+
+  public Task() {
+  }
+
+  public Task(String title, String description, LocalDate dueDate, Priority priority, Status status) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = status;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public LocalDate getDueDate() {
+    return dueDate;
+  }
+
+  public void setDueDate(LocalDate dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  public Priority getPriority() {
+    return priority;
+  }
+
+  public void setPriority(Priority priority) {
+    this.priority = priority;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
 }
---
+++ src/entities/enums/Status.java
@@ -0,0 +1,7 @@
+package entities.enums;
+
+public enum Status {
+  PENDING,
+  ONGOING,
+  COMPLETED;
+}
