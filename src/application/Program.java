b676fca90bdb02b5f6f7e2dad390b3336878c62c
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-05-25 17:20:24 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Program {
+  public static void main(String[] args) {
+    System.out.println("Tasks: ");
+    System.out.println();
+    System.out.println("what you what to do? ");
+    System.out.println("Add Tasks (a)");
+    System.out.println("Remove Tasks (r)");
+  }
+}
