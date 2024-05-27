cb6c7eaa0638af288495270d786a65e141af007a
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-05-27 19:07:33 -0300

    add exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,11 @@ package application;
 import entities.Task;
 import entities.enums.Priority;
 import entities.enums.Status;
+import entities.exceptions.TaskException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.*;
 
 public class Program {
@@ -15,58 +17,91 @@ public class Program {
     List<Task> tasks = new ArrayList<>();
 
     while (true) {
-      System.out.println();
-      System.out.println("#Task Manager:");
-      System.out.println("What you what to do? ");
-      System.out.println("Add Tasks (a)");
-      System.out.println("Remove Tasks (r)");
-      System.out.println("Update Tasks (u)");
-      char command = sc.nextLine().charAt(0);
-      String description = null;
-      Date dueDate = null;
-      SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-      if (command == 'a') {
-        System.out.println("Add Task:");
-        System.out.print("Title: ");
-        String title = sc.nextLine();
-        System.out.print("Description? (y/n) ");
-        if (sc.nextLine().charAt(0) == 'y') {
-          System.out.print("Description: ");
-          description = sc.nextLine();
-        }
-        System.out.print("Due Date? (y/n) ");
-        if (sc.nextLine().charAt(0) == 'y') {
-          System.out.print("Due Date (dd/mm/yyyy): ");
-          dueDate = sdf.parse(sc.nextLine());
-        }
-        System.out.print("Priority (low/medium/high): ");
-        String priority = sc.nextLine().toUpperCase();
-        System.out.print("Status (pending/ongoing/completed): ");
-        String status = sc.nextLine().toUpperCase();
-        Task task = new Task(title, description, dueDate, Priority.valueOf(priority), Status.valueOf(status));
-        int position = 0;
-        while (position < tasks.size() && (tasks.get(position).getStatus() != Status.COMPLETED)) {
-          position++;
-        }
-        tasks.add(position, task);
+      try {
         System.out.println();
-        System.out.println("-------------------------------------------------");
-        System.out.println();
-      } else if (command == 'r') {
-        System.out.print("Which task you what to delete (number)? ");
-        tasks.remove(sc.nextInt() - 1);
+        System.out.println("#Task Manager:");
+        System.out.println("What you what to do? ");
+        System.out.println("Add Tasks (a)");
+        System.out.println("Remove Tasks (r)");
+        System.out.println("Update Tasks (u)");
+        char command = sc.nextLine().charAt(0);
+        String description = null;
+        Date dueDate = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        if (command == 'a') {
+          System.out.println("Add Task:");
+          System.out.print("Title: ");
+          String title = sc.nextLine();
+          System.out.print("Description? (y/n) ");
+          if (sc.nextLine().charAt(0) == 'y') {
+            System.out.print("Description: ");
+            description = sc.nextLine();
+          }
+          System.out.print("Due Date? (y/n) ");
+          if (sc.nextLine().charAt(0) == 'y') {
+            System.out.print("Due Date (dd/mm/yyyy): ");
+            dueDate = sdf.parse(sc.nextLine());
+          }
+          System.out.print("Priority (low/medium/high): ");
+          String priority = sc.nextLine().toUpperCase();
+          if (!priority.equals("LOW") && !priority.equals("MEDIUM") && !priority.equals("HIGH")) {
+            throw new TaskException("Invalid Priority! valid priorities are (low/medium/high)");
+          }
+          System.out.print("Status (pending/ongoing/completed): ");
+          String status = sc.nextLine().toUpperCase();
+          if (!status.equals("PENDING") && !status.equals("ONGOING") && !status.equals("COMPLETED")) {
+            throw new TaskException("Invalid Status! valid status are (pending/ongoing/completed)");
+          }
+          Task task = new Task(title, description, dueDate, Priority.valueOf(priority), Status.valueOf(status));
+          int position = 0;
+          while (position < tasks.size() && (tasks.get(position).getStatus() != Status.COMPLETED)) {
+            position++;
+          }
+          tasks.add(position, task);
+          System.out.println();
+          System.out.println("-------------------------------------------------");
+          System.out.println();
+        } else if (!tasks.isEmpty()) {
+          if (command == 'r') {
+            System.out.print("Which task you what to delete (number)? ");
+            int removeChoice = sc.nextInt();
+            if (removeChoice > tasks.size() || removeChoice < tasks.size()) {
+              throw new TaskException("You task list just have " + tasks.size() + " task(s)");
+            }
+            tasks.remove(removeChoice - 1);
+            sc.nextLine();
+            System.out.println();
+          } else if (command == 'u') {
+            System.out.print("Which task you what to update (number)? ");
+            int taskNumber = sc.nextInt();
+            if (taskNumber > tasks.size() || taskNumber < tasks.size()) {
+              throw new TaskException("You task list just have " + tasks.size() + " task(s)");
+            }
+            sc.nextLine();
+            System.out.println("Update task " + taskNumber + ":");
+            System.out.print("what do you what to update (title/description/duedate/priority/status)? ");
+            String updateDecision = sc.nextLine();
+            if (!updateDecision.equals("title") && !updateDecision.equals("description") && !updateDecision.equals("duedate") && !updateDecision.equals("priority") && !updateDecision.equals("status")) {
+              throw new TaskException("Invalid Value!");
+            }
+            testUpdate(updateDecision, tasks, taskNumber);
+          } else {
+            throw new TaskException("Invalid command! valid commands are (a/r/u)");
+          }
+        } else {
+          throw new TaskException("You dont have tasks!");
+        }
+        UI.printAll(tasks, sdf);
+      } catch (TaskException e) {
+        System.out.println(e.getMessage());
         sc.nextLine();
-        System.out.println();
-      } else if (command == 'u') {
-        System.out.print("Which task you what to update (number)? ");
-        int taskNumber = sc.nextInt();
+      } catch (ParseException e) {
+        System.out.println("Invalid Date!");
         sc.nextLine();
-        System.out.println("Update task " + taskNumber + ":");
-        System.out.print("what do you what to update (title/description/duedate/priority/status)? ");
-        String updateDecision = sc.nextLine();
-        testUpdate(updateDecision, tasks, taskNumber);
+      } catch (RuntimeException e) {
+        System.out.println("Invalid error!");
+        sc.nextLine();
       }
-      UI.printAll(tasks, sdf);
     }
   }
 
@@ -86,6 +121,9 @@ public class Program {
       tasks.get(taskNumber - 1).setDescription(tye);
       System.out.println();
     } else if (updateDecision.equals("duedate")) {
+      if (tasks.get(taskNumber - 1).getDueDate() == null) {
+        tasks.get(taskNumber - 1).setDueDate(Date.from(Instant.now()));
+      }
       System.out.println("Actual Due date: " + sdf.format(tasks.get(taskNumber - 1).getDueDate()));
       System.out.print("New Due date: ");
       Date tyc = sdf.parse(sc.nextLine());
@@ -94,13 +132,21 @@ public class Program {
     } else if (updateDecision.equals("priority")) {
       System.out.println("Actual Priority: " + tasks.get(taskNumber - 1).getPriority());
       System.out.print("New Priority: ");
-      Priority tye = Priority.valueOf(sc.nextLine().toUpperCase());
+      String priority = sc.nextLine().toUpperCase();
+      if (!priority.equals("LOW") && !priority.equals("MEDIUM") && !priority.equals("HIGH")) {
+        throw new TaskException("Invalid Priority! valid priorities are (low/medium/high)");
+      }
+      Priority tye = Priority.valueOf(priority);
       tasks.get(taskNumber - 1).setPriority(tye);
       System.out.println();
     } else if (updateDecision.equals("status")) {
       System.out.println("Actual Status: " + tasks.get(taskNumber - 1).getStatus());
       System.out.print("New Status: ");
-      Status tye = Status.valueOf(sc.nextLine().toUpperCase());
+      String status = sc.nextLine().toUpperCase();
+      if (!status.equals("PENDING") && !status.equals("ONGOING") && !status.equals("COMPLETED")) {
+        throw new TaskException("Invalid Status! valid status are (pending/ongoing/completed)");
+      }
+      Status tye = Status.valueOf(status);
       tasks.get(taskNumber - 1).setStatus(tye);
       System.out.println();
     }
---
+++ src/entities/exceptions/TaskException.java
@@ -0,0 +1,7 @@
+package entities.exceptions;
+
+public class TaskException extends RuntimeException {
+  public TaskException(String message) {
+    super(message);
+  }
+}
